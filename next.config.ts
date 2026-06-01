e7fb0f80bb339538ee5faae4cde92e05858090de
alexyev <alexanderyevchenko@gmail.com>
2026-06-01 16:52:08 -0300

    change configs for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'covers.openlibrary.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'books.google.com',
-      },
-      {
-        protocol: 'http',
-        hostname: 'books.google.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'substackcdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'substack-post-media.s3.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'miro.medium.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-    unoptimized: false,
-  },
 };
 
 export default nextConfig;
